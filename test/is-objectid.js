a1a63d420527aff4ca6a2e192201691f7841226f
isayme <isaymeorg@gmail.com>
2019-10-17 20:56:01 +0800

    upgrade deps

--- test/is-objectid.js
+++ test/is-objectid.js
@@ -1,4 +1,4 @@
-const assert = require('assert')
+const assert = require('power-assert')
 const isObjectId = require('../lib/is-objectid')
 
 describe('isObjectId', () => {
--- test/scalar-objectid.js
+++ test/scalar-objectid.js
@@ -1,4 +1,4 @@
-const assert = require('assert')
+const assert = require('power-assert')
 const {
   graphql,
   isScalarType,
